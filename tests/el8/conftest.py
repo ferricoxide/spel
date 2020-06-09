30206f917eb5590f0a498cc3336e8b07e56ef647
Thomas H Jones II <github@xanthia.com>
2020-06-09 15:08:55 +0000

    Dupe el8 content from el7

---
+++ tests/el8/conftest.py
@@ -0,0 +1,62 @@
+import os
+
+import distro
+import pytest
+from six.moves import urllib
+
+# Static globals
+METADATA_KERNEL = 'http://169.254.169.254/latest/meta-data/kernel-id'
+FIPS_DISABLED = set(['true', 'TRUE', '1', 'on'])
+
+# Markers
+VIRTUALIZATION_MARKERS = set(['hvm', 'paravirtual'])
+PLAT_MARKERS = set(['el7'])
+FIPS_MARKERS = set(['fips_enabled', 'fips_disabled'])
+
+# Platform-specific globals
+PLAT = 'el' + distro.major_version()
+FIPS = 'fips_disabled' if os.environ.get('SPEL_DISABLEFIPS') in FIPS_DISABLED \
+    else 'fips_enabled'
+VIRT = 'hvm'
+try:
+    urllib.request.urlopen(METADATA_KERNEL)
+    VIRT = 'paravirtual'
+except urllib.error.URLError:
+    pass
+
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "el7: mark test to run only on el7 platforms"
+    )
+    config.addinivalue_line(
+        "markers", "hvm: mark test to run only on hvm instances"
+    )
+    config.addinivalue_line(
+        "markers", "paravirtual: mark test to run only on paravirtual instances"
+    )
+    config.addinivalue_line(
+        "markers", "fips_enabled: mark test to run only if fips is enabled"
+    )
+    config.addinivalue_line(
+        "markers", "fips_disabled: mark test to run only if fips is disabled"
+    )
+
+
+def pytest_runtest_setup(item):
+    if isinstance(item, pytest.Function):
+        if not item.get_closest_marker(PLAT):
+            if PLAT_MARKERS.intersection(item.keywords):
+                pytest.skip('does not run on platform {0}'.format(PLAT))
+        if not item.get_closest_marker(VIRT):
+            if VIRTUALIZATION_MARKERS.intersection(item.keywords):
+                pytest.skip(
+                    'does not run on virtualization type {0}'.format(VIRT))
+        if not item.get_closest_marker(FIPS):
+            if FIPS_MARKERS.intersection(item.keywords):
+                pytest.skip(
+                    'test incompatible with fips mode, {0}'.format(FIPS))
+
+
+def pytest_logger_stdoutloggers(item):
+    return ['spel_validation']
---
+++ tests/el8/test_ami.py
@@ -0,0 +1,170 @@
+import logging
+
+import pytest
+
+log = logging.getLogger('spel_validation')
+log.setLevel(logging.INFO)
+
+
+@pytest.mark.hvm
+def test_10_gigabit(host):
+    interface = host.interface('eth0')
+    assert interface.speed == 10000
+
+
+def test_root_volume_is_resized(host):
+    cmd = 'test $(vgs --noheadings -o pv_free | sed \'s/ //g\') != 0'
+    pv_free = host.run(cmd)
+    assert pv_free.exit_status == 0
+
+
+@pytest.mark.parametrize("name", [
+    ("aws-amitools-ec2"),
+    ("aws-apitools-as"),
+    ("aws-apitools-cfn"),
+    ("aws-apitools-common"),
+    ("aws-apitools-ec2"),
+    ("aws-apitools-elb"),
+    ("aws-apitools-iam"),
+    ("aws-apitools-mon"),
+    ("aws-apitools-rds"),
+    ("aws-cfn-bootstrap")
+])
+def test_common_aws_pkgs(host, name):
+    pkg = host.package(name)
+    if pkg.is_installed:
+        log.info(
+            '%s',
+            {'pkg': pkg.name, 'version': pkg.version, 'release': pkg.release})
+    assert pkg.is_installed
+
+
+@pytest.mark.el7
+@pytest.mark.parametrize("name", [
+    ("aws-scripts-ses")
+])
+def test_el7_aws_pkgs(host, name):
+    pkg = host.package(name)
+    if pkg.is_installed:
+        log.info(
+            '%s',
+            {'pkg': pkg.name, 'version': pkg.version, 'release': pkg.release})
+    assert pkg.is_installed
+
+
+def test_aws_cli_is_in_path(host):
+    cmd = 'aws --version'
+    aws = host.run(cmd)
+    log.info('\n%s', aws.stderr)
+    assert host.exists('aws')
+
+
+def test_repo_access(host):
+    cmd = 'yum repolist all 2> /dev/null | sed -n \'/^repo id/,$p\''
+    repos = host.run(cmd)
+    log.info('stdout:\n%s', repos.stdout)
+    log.info('stderr:\n%s', repos.stderr)
+    assert repos.exit_status == 0
+
+
+def test_boot_is_mounted(host):
+    boot = host.mount_point('/boot')
+    assert boot.exists
+
+
+def test_tmp_mount_properties(host):
+    tmp = host.mount_point('/tmp')
+    assert tmp.exists
+    assert tmp.device == 'tmpfs'
+    assert tmp.filesystem == 'tmpfs'
+
+
+@pytest.mark.el7
+def test_el7_selinux_enforcing(host):
+    cmd = 'test $(getenforce) = \'Enforcing\''
+    selinux_permissive = host.run(cmd)
+    assert selinux_permissive.exit_status == 0
+
+
+@pytest.mark.el7
+@pytest.mark.fips_enabled
+def test_el7_fips_enabled(host):
+    fips = host.file('/proc/sys/crypto/fips_enabled')
+    assert fips.exists and fips.content.strip() == b'1'
+
+
+@pytest.mark.el7
+@pytest.mark.fips_disabled
+def test_el7_fips_disabled(host):
+    fips = host.file('/proc/sys/crypto/fips_enabled')
+    assert not fips.exists or fips.content.strip() == b'0'
+
+
+@pytest.mark.parametrize("names", [
+    ([
+        'python3',
+        'python36',
+    ])
+])
+def test_python3_installed(host, names):
+    pkg = type('pkg', (object,), {'is_installed': False})
+    for name in names:
+        pkg = host.package(name)
+        if pkg.is_installed:
+            break
+
+    assert pkg.is_installed
+    log.info(
+        '%s',
+        {'pkg': pkg.name, 'version': pkg.version, 'release': pkg.release})
+
+
+@pytest.mark.el7
+@pytest.mark.parametrize("realpath,link", [
+    ('/usr/bin/python3.6', '/usr/bin/python3')
+])
+def test_python3_symlink(host, realpath, link):
+    python3_symlink = host.file(link).linked_to
+    assert python3_symlink == realpath
+
+
+@pytest.mark.parametrize("version", [
+    ('3.6')
+])
+def test_python3_version(host, version):
+    cmd = 'python3 --version'
+    python3_version = host.run(cmd)
+    log.info('`%s` stdout: %s', cmd, python3_version.stdout)
+    log.info('`%s` stderr: %s', cmd, python3_version.stderr)
+
+    assert python3_version.exit_status == 0
+
+    # Example stdout content: 'Python 3.6.8'
+    assert python3_version.stdout.strip().split()[1].startswith(version)
+
+
+@pytest.mark.el7
+def test_timedatectl_dbus_status(host):
+    cmd = 'timedatectl'
+    timedatectl = host.run(cmd)
+    log.info('stdout:\n%s', timedatectl.stdout)
+    log.info('stderr:\n%s', timedatectl.stderr)
+    assert timedatectl.exit_status == 0
+
+
+@pytest.mark.el7
+def test_var_run_symlink(host):
+    var_run_symlink = host.file('/var/run').linked_to
+    assert var_run_symlink == '/run'
+
+
+@pytest.mark.parametrize("service", [
+    ("autotune.service"),
+    ("amazon-ssm-agent.service"),
+    ("hibinit-agent.service"),
+    ("ec2-instance-connect.service")
+])
+
+def test_systemd_services(host, service):
+    chk_service = host.service(service)
+    assert chk_service.is_enabled
